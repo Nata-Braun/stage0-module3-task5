70e743d0756345d8e4d36dc219e40e75a1d3207c
Nata Braun <pushistik2030@gmail.com>
2023-02-02 18:43:03 +0300

    Update readme.md

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float toBeDivided1 = toBeDivided;
+        float result = toBeDivided1/5;
+        System.out.println(result);
     }
 }
